6e1c5e187d8ef63ab94a8fcb3140180bff79da5e
devendra1608 <151811312+devendra1608@users.noreply.github.com>
2024-11-16 11:04:50 +0530

    Frontend Added

---
+++ DBMS/src/pages/CustomerLogin.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./LoginPage.css";
+const CustomerLogin = () => {
+  return (
+    <div className="login-container">
+      <header className="login-header">
+        <img src="/path/to/logo.png" alt="Company Logo" className="login-logo" />
+        <h1>Company Name</h1>
+      </header>
+      <div className="login-box">
+        <h2>LOGIN</h2>
+        <div className="input-group">
+          <input type="text" placeholder="Username" className="login-input" />
+          <span className="icon">👤</span>
+        </div>
+        <div className="input-group">
+          <input type="password" placeholder="Password" className="login-input" />
+          <span className="icon">🔑</span>
+        </div>
+        <div className="login-options">
+          <label>
+            <input type="checkbox" /> Remember Me
+          </label>
+          <a href="/forgot-password" className="forgot-password">Forgot Password?</a>
+        </div>
+        <button className="login-button">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerLogin;
