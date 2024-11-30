d832cf33c077a2a014c28deefb7132a8523f1582
Hemang Mishra <hemangmishranew@gmail.com>
2024-11-30 18:42:21 +0530

    Fixed bugs

--- farmer_dashboard.php
+++ farmer_dashboard.php
@@ -72,11 +72,6 @@ $stats = $result_stats->fetch_assoc();
         </div>
     </div>
 
-    <!-- Add Crop Button -->
-    <div class="add-crop-btn-container">
-        <a href="add_crop.php" class="add-crop-btn">Add Crop</a>
-    </div>
-
 
     <!-- Crops Section -->
     <div class="crops">
